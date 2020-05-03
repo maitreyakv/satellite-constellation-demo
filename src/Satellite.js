da9b9fff3d8acf40f963df20d9f724d1f1e9fdcd
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-03 16:05:27 -0400

    Added orbit controls and refactored Satellite class

--- src/Satellite.js
+++ src/Satellite.js
@@ -11,7 +11,7 @@ const R_earth = 6.371009e3; // km
 // TODO: Add doc for class
 class Satellite {
     // Constructor for Satellite class
-    constructor(a, e, i, omega, Omega, nu_0) {
+    constructor(a, e, i, omega, Omega, nu_0, scene) {
         // Initialize semi-major-axis length
         this.a = a;
 
@@ -31,19 +31,34 @@ class Satellite {
         this.mean_motion = Math.sqrt(mu / Math.pow(a, 3));
 
         // Compute eccentric anomaly at start epoch
-        var E_0 = 2.0 * Math.atan(Math.sqrt((1 - e)/(1 + e)) * Math.tan(nu_0 / 2.0));
+        const E_0 = 2.0 * Math.atan(Math.sqrt((1 - e)/(1 + e)) * Math.tan(nu_0 / 2.0));
 
         // Compute mean anomaly at start epoch
         this.M_0 = E_0 - e * Math.sin(E_0);
+
+        // Satellite geometry in renderiing
+        const geometry = new THREE.SphereGeometry(0.05 * R_earth, 32, 32);
+
+        // Satellite material in rendering
+        const material = new THREE.MeshLambertMaterial( { color: 0x999999 } );
+
+        // Satellite mesh in rendering
+        this.mesh = new THREE.Mesh(geometry, material);
+
+        // Add the satellite mesh to the scene
+        scene.add(this.mesh);
+
+        // Plot the orbit of the satellite
+        this.plotOrbit(scene);
     }
 
     // TODO: Add doc for function
     position(t) {
         // Compute mean anomaly at the queried time
-        var M = (this.M_0 + t * this.mean_motion) % (2.0 * Math.PI);
+        const M = (this.M_0 + t * this.mean_motion) % (2.0 * Math.PI);
 
         // Obtain the eccentricity as a local variable
-        var e = this.e;
+        const e = this.e;
 
         // Define the N-R iteration function and its derivative
         function f(Ek) { return Ek - e * Math.sin(Ek) - M; }
@@ -60,32 +75,58 @@ class Satellite {
         }
 
         // Compute true anomaly at the queried time
-        var nu = 2.0 * Math.atan(Math.sqrt((1.0 + e)/(1.0 - e)) * Math.tan(Ek / 2.0));
+        const nu = 2.0 * Math.atan(Math.sqrt((1.0 + e)/(1.0 - e)) * Math.tan(Ek / 2.0));
 
         // Get position vector in perfical reference frame
-        var r_pqw_ = new THREE.Vector3(Math.cos(nu), Math.sin(nu), 0.0).multiplyScalar(
+        const r_pqw_ = new THREE.Vector3(Math.cos(nu), Math.sin(nu), 0.0).multiplyScalar(
                         this.a * (1.0 - Math.pow(this.e, 2) / (1.0 + this.e * Math.cos(nu))) );
 
 
         // Construct inverted rotation matrices from perfical frame to earth inertial frame
-        var R3_Omega_inv = new THREE.Matrix3().set(
+        const R3_Omega_inv = new THREE.Matrix3().set(
                                 Math.cos(this.Omega) , -Math.sin(this.Omega), 0.0,
                                 Math.sin(this.Omega) , Math.cos(this.Omega) , 0.0,
                                 0.0                  , 0.0                  , 1.0);
-        var R1_i_inv = new THREE.Matrix3().set(
+        const R1_i_inv = new THREE.Matrix3().set(
                                 1.0                  , 0.0                  , 0.0,
                                 0.0                  , Math.cos(this.i)     , -Math.sin(this.i),
                                 0.0                  , Math.sin(this.i)     , Math.cos(this.i));
-        var R3_omega_inv = new THREE.Matrix3().set(
+        const R3_omega_inv = new THREE.Matrix3().set(
                                 Math.cos(this.omega) , -Math.sin(this.omega), 0.0,
                                 Math.sin(this.omega) , Math.cos(this.omega) , 0.0,
                                 0.0                  , 0.0                  , 1.0);
 
         // Convert the position from the perifocal frame to the Earth centered inertial frame
-        var r_eci_ = r_pqw_.clone();
+        const r_eci_ = r_pqw_.clone();
         r_eci_.applyMatrix3(R3_omega_inv).applyMatrix3(R1_i_inv).applyMatrix3(R3_Omega_inv);
 
         // Return the position vector in the Earth centered inertial frame
         return r_eci_;
+    }
+
+    // TODO: Add doc for function
+    updatePosition(t) {
+        // Update the position of the satellite in the scene
+        this.mesh.position.copy(this.position(t));
+    }
+
+    // TODO: Add doc for this function
+    plotOrbit(scene) {
+        // Compute period of the orbit
+        const T = 2.0 * Math.PI * Math.sqrt(Math.pow(this.a, 3) / mu);
+
+        // Compute the orbit of the satellite as an array of positions
+        const points = [];
+        for (var t = 0; t < T; t += T / 100.0) {
+            points.push(this.position(t));
+        }
+
+        // Create a curve for the orbit
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+        const material = new THREE.LineBasicMaterial( { color : 0xffffff } );
+        const orbit = new THREE.Line( geometry, material );
+
+        // Add the orbit to the scene
+        scene.add(orbit);
     }
 }
--- src/main.js
+++ src/main.js
@@ -9,41 +9,48 @@ const scene = new THREE.Scene();
 
 // Create Camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100.0 * R_earth );
-camera.position.z = 4.0 * R_earth;
-camera.position.y = 1.0 * R_earth;
-camera.lookAt(0,0,0);
+camera.up.set(0,0,1);
 scene.add(camera);
+
+// Orbit controls
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+camera.position.y = 4.0 * R_earth;
+camera.position.z = 1.0 * R_earth;
+camera.lookAt(0,0,0);
+controls.update();
 
 // Earth Mesh
 const earth_geom = new THREE.SphereGeometry(R_earth, 32, 32);
-const earth_mat = new THREE.MeshPhongMaterial( {color: 0x999999} );
+const earth_mat = new THREE.MeshNormalMaterial();
 const earth_mesh = new THREE.Mesh(earth_geom, earth_mat);
 scene.add(earth_mesh);
 
+// Axes of intertial reference frame
+const axesHelper = new THREE.AxesHelper(2.0 * R_earth);
+scene.add( axesHelper );
+
 // Satellite Mesh
-const sat = new Satellite(2.0 * R_earth, 0.0, Math.PI/2.0, 0.0, 0.0, 0.0);
-const sat_geom = new THREE.SphereGeometry(0.1 * R_earth, 32, 32);
-const sat_mat = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
-const sat_mesh = new THREE.Mesh(sat_geom, sat_mat);
-scene.add(sat_mesh);
+const sat1 = new Satellite(2.0 * R_earth, 0.0, 0.0, 0.0, 0.0, 0.0, scene);
+const sat2 = new Satellite(2.0 * R_earth, 0.0, 0.0, 0.0, 0.0, Math.PI, scene);
 
 // Ambient Light Source
 const ambient_light = new THREE.AmbientLight(0xf1f1f1, 1);
 scene.add(ambient_light);
 
 // Spot Light Source
-const spot_light = new THREE.DirectionalLight(0xffffff);
-spot_light.position.set(100*R_earth,100*R_earth,100*R_earth);
-scene.add(spot_light);
+//const spot_light = new THREE.DirectionalLight(0xffffff);
+//spot_light.position.set(100*R_earth,100*R_earth,100*R_earth);
+//scene.add(spot_light);
 
 // Render loop
 var t = 0.0;
 function render() {
-    t += 10.0;
+    t += 50.0;
     requestAnimationFrame(render);
-    sat_mesh.position.copy(sat.position(t));
+    sat1.updatePosition(t);
+    sat2.updatePosition(t);
 
-
+    controls.update();
     renderer.render(scene, camera);
 }
 render();
